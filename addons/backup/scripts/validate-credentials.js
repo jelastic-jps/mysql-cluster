b898e6f81a1e19dd0ebe49d2e10b96043eae2e00
Ruslan Synytsky <siruslan@gmail.com>
2017-11-12 14:44:20 +0100

    + credentials validation script

---
+++ addons/backup/scripts/validate-credentials.js
@@ -0,0 +1,34 @@
+//@req(service)
+
+if (service == 'db') {
+    cmd = 'mysql -u ${settings.db_user} -p${settings.db_password} -e "quit"'
+    mark = 'Access denied'
+    warning = 'DB User and Password: authentication check failed. Please specify correct credentials for the database located in node${this.id}.'
+    return Check(cmd, mark, warning)
+} 
+
+if (service == 's3') {
+    cmd = "'rpm -qa | grep -qw s3cmd || yum install -y s3cmd'; s3cmd ls --access_key=${settings.access_key} --secret_key=${settings.access_key} --host=${settings.host} --no-check-hostname'"
+    mark = 'Access denied'
+    warning = 'S3 Credentials: authentication check failed. Please specify correct host and credentials for S3 storage.'
+    return Check(cmd, mark, warning)
+}
+
+return {result: 99, error: 'Service + [' + service + '] not found'}
+
+function Check(cmd, mark, warning){
+    resp = ExecCmd(cmd)
+    if (resp.result != 0) {
+        if (resp.responses[0].errOut.indexOf(mark) > -1) {
+            return {
+                result: 'warning',
+                message: warning
+            }
+        } else return resp
+    }
+    return {result: 0}
+}
+
+function ExecCmd(cmd){
+    return jelastic.env.control.ExecCmdById('${env.envName}', session, '${this.id}', toJSON([{command: cmd}]), true, 'jelastic');
+} 
