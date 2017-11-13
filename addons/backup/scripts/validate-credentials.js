f586ac738e6a8e1a47e1fc1e9081bdd689b5f689
Ruslan Synytsky <siruslan@gmail.com>
2017-11-13 13:56:03 +0100

    + commands separator

--- addons/backup/scripts/validate-credentials.js
+++ addons/backup/scripts/validate-credentials.js
@@ -1,7 +1,7 @@
 //@req(service, nodeId)
 
 if (service == 'db') {
-    cmd = '[ -x "$(command -v mysql)" ] && { mysql -u ${settings.db_user} -p${settings.db_password} -e "quit"; } || { :; }'; 
+    cmd = '[ -x "$(command -v mysql)" ] && { mysql -u ${settings.db_user} -p${settings.db_password} -e "quit"; } || { :; }; '; 
     cmd += '[ -x "$(command -v psql)" ] && { export PGPASSWORD=${settings.db_password}; psql -U ${settings.db_user} -d postgres -c "\\q"; } || { :; }'
     mark = ['Access denied', 'authentication failed']
     warning = 'DB User and Password: authentication check failed. Please specify correct credentials for the database located in node' + nodeId + '.'
