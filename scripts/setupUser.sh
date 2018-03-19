807e9d1f4b6e3f1dd75cbb32dfe9baa2118cb125
sych74 <sych74@gmail.com>
2018-03-19 14:51:23 +0200

    Enable mysql8

--- scripts/setupUser.sh
+++ scripts/setupUser.sh
@@ -12,18 +12,18 @@ resp=$(mysql -u$user -p$pswd mysql --execute="SHOW COLUMNS FROM user")
 	cmd="CREATE TEMPORARY TABLE tmptable SELECT * FROM user WHERE User = 'root'; UPDATE tmptable SET User = '$user' WHERE User = 'root'; DELETE FROM user WHERE User = '$user'; INSERT INTO user SELECT * FROM tmptable WHERE User = '$user'; DROP TABLE tmptable;"
 	mysql mysql --execute="$cmd"
 
-	version=$(mysql -V | awk '{print $5}')
-	major=$(echo $version | cut -d '.' -f1)
-	minor=$(echo $version | cut -d '.' -f2)
-	[ "$major" -ge 5 -a "$minor" -ge 7 ] && {
-		cmd="UPDATE user SET authentication_string=PASSWORD('$pswd') WHERE user='$user';";
-	} || {
+	version=$(mysql --version|awk '{ print $5 }'|awk -F\, '{ print $1 }')
+	
+	if (( $(awk 'BEGIN {print ("'$version'" >= "'5.7'")}') )); then
+ 		cmd="UPDATE user SET authentication_string=PASSWORD('$pswd') WHERE user='$user';";
+	else
 		cmd="UPDATE user SET password=PASSWORD('$pswd') WHERE user='$user';";
-	}
+	fi
 
 	mysql mysql --execute="$cmd"
 	echo $resp
 	rm -f /var/lib/mysql/auto.cnf
 	service mysql restart
+} || { 
+	echo "[Info] User $user has the required access to the database." 
 }
-
