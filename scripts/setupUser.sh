ad4790705e6ead5473a91f3dc7bba29799fc3756
unigeocloud <unigeocloud@gmail.com>
2019-07-09 10:34:03 +0300

    Update setupUser.sh

--- scripts/setupUser.sh
+++ scripts/setupUser.sh
@@ -1,43 +1,16 @@
 #!/bin/bash
 
-user=$1
-pswd=$2
-
-
-waiting_MYSQL_service() {
-        local LOOP_LIMIT=60
-                for (( i=0 ; i<${LOOP_LIMIT} ; i++ )); do
-                        echo "${i} - alive?..\n"
-                        mysqladmin -u$user -p$pswd ping | grep 'mysqld is alive' > /dev/null 2>&1;
-                        [ $? == 0 ] && break;
-                        sleep 1
-    		done
-}
-
-
-
+USER=$1
+PASSWORD=$2
+ADMIN_PASSWORD=`pwgen 10 1`
+JEM=`which jem`
+MYSQL=`which mysql`
+cmd="CREATE USER '$USER'@'localhost' IDENTIFIED BY '$PASSWORD'; CREATE USER '$USER'@'%' IDENTIFIED BY '$PASSWORD'; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'localhost'; GRANT ALL PRIVILEGES ON *.* TO '$USER'@'%'; FLUSH PRIVILEGES;"
 unset resp;
-resp=$(mysql -u$user -p$pswd mysql --execute="SHOW COLUMNS FROM user")
+resp=$(mysql -u$USER -p$PASSWORD mysql --execute="SHOW COLUMNS FROM user")
 [ -z "$resp" ] && {
-	service mysql stop;
-	mysqld_safe --skip-grant-tables --user=mysql --pid-file=/var/lib/mysql/mysqld.pid &
-	waiting_MYSQL_service;
-	cmd="CREATE TEMPORARY TABLE tmptable SELECT * FROM user WHERE User = 'root'; UPDATE tmptable SET User = '$user' WHERE User = 'root'; DELETE FROM user WHERE User = '$user'; INSERT INTO user SELECT * FROM tmptable WHERE User = '$user'; DROP TABLE tmptable;"
-	mysql mysql --execute="$cmd"
-
-	version=$(mysql --version|awk '{ print $5 }'|awk -F\, '{ print $1 }')
-
-	if (( $(awk 'BEGIN {print ("'$version'" >= "'5.7'")}') )); then
-# 		cmd="UPDATE user SET authentication_string=PASSWORD('$pswd') WHERE user='$user';";
-		cmd="FLUSH PRIVILEGES; ALTER USER '$user'@'%' IDENTIFIED WITH mysql_native_password BY '$pswd'; ALTER USER '$user'@'localhost' IDENTIFIED WITH mysql_native_password BY '$pswd'; GRANT ALL ON *.* TO '$user'@'%';GRANT ALL ON *.* TO '$user'@'localhost'; FLUSH PRIVILEGES;";
-	else
-		cmd="UPDATE user SET password=PASSWORD('$pswd') WHERE user='$user'; GRANT ALL ON *.* TO '$user'@'%';GRANT ALL ON *.* TO '$user'@'localhost'; FLUSH PRIVILEGES;";
-	fi
-
-	mysql mysql --execute="$cmd"
-	echo $resp
-	rm -f /var/lib/mysql/auto.cnf
-	service mysql restart
+   $JEM passwd set -p $ADMIN_PASSWORD
+   $MYSQL -uroot -p${ADMIN_PASSWORD} --execute="$cmd"
 } || {
-	echo "[Info] User $user has the required access to the database."
+   echo "[Info] User $user has the required access to the database."
 }
