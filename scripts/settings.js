4fb47233e3e1877c77a2406fcb694b90bac20f95
sych74 <sych74@gmail.com>
2021-02-04 20:31:32 +0200

    Added quotas for galera cluster

--- scripts/settings.js
+++ scripts/settings.js
@@ -4,11 +4,14 @@ import com.hivext.api.core.utils.Transport;
 
 //checking quotas
 var perEnv = "environment.maxnodescount",
-      perNodeGroup = "environment.maxsamenodescount";
-var nodesPerEnvMin = 2,
-      nodesPerEnvMinWithProxy = 4,
-      nodesPerGroupMin = 2,
-      markup = "", cur = null, text = "used", install = true;
+    perNodeGroup = "environment.maxsamenodescount";
+var nodesPerEnvSlaveMin = 2,
+    nodesPerEnvGaleraMin = 3,
+    nodesPerEnvSlaveMinWithProxy = 4,
+    nodesPerEnvGaleraMinWithProxy = 5,
+    nodesPerGroupSlaveMin = 2,
+    nodesPerGroupGaleraMin = 3,
+    markup = "", cur = null, text = "used", install = true;
       
 var settings = jps.settings;
 var fields = {};
@@ -19,18 +22,18 @@ var quotas = jelastic.billing.account.GetQuotas(perEnv + ";"+perNodeGroup ).arra
 for (var i = 0; i < quotas.length; i++){
   var q = quotas[i], n = toNative(q.quota.name);
 
-  if (n == perEnv && nodesPerEnvMin > q.value){
-    err(q, "required", nodesPerEnvMin, true);
+  if (n == perEnv && nodesPerEnvSlaveMin > q.value){
+    err(q, "required", nodesPerEnvSlaveMin, true);
     install = false;
   }
     
-  if (n == perNodeGroup && nodesPerGroupMin > q.value){
-    if (!markup) err(q, "required", nodesPerGroupMin, true);
+  if (n == perNodeGroup && nodesPerGroupSlaveMin > q.value){
+    if (!markup) err(q, "required", nodesPerGroupSlaveMin, true);
     install = false;
   }
 
-  if (n == perEnv && nodesPerEnvMinWithProxy  > q.value){
-    if (!markup) err(q, "required", nodesPerEnvMinWithProxy, true);
+  if (n == perEnv && nodesPerEnvSlaveMinWithProxy  > q.value){
+    if (!markup) err(q, "required", nodesPerEnvSlaveMinWithProxy, true);
     fields["is_proxysql"].value = false;
     fields["is_proxysql"].disabled = true;
     fields["message"].markup = "ProxySQL is not available. " + markup + "Please upgrade your account.";
@@ -38,6 +41,11 @@ for (var i = 0; i < quotas.length; i++){
     fields["message"].hidden = false;
     fields["message"].height = 30;      
   }
+ 
+  if (n == perEnv && nodesPerEnvGaleraMinWithProxy > q.value) || (n == perNodeGroup && nodesPerGroupGaleraMin > q.value) {
+    fields["scheme"].dependsOn.stack["mariadb-dockerized"].splice(-1);
+  }      
+ 
 }
 
 if (!install) {
