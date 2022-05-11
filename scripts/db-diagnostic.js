3e5e16f97e94d60d716ec50b433b7704eb04fb90
alexey.lazarenko <dzotic9@gmail.com>
2022-05-11 17:00:17 +0300

    Update db-diagnostic.js

--- scripts/db-diagnostic.js
+++ scripts/db-diagnostic.js
@@ -1,33 +1,146 @@
-var SH_SCRIPT = "bash /home/jelastic/diagnostic.sh",
-    SQLDB = "sqldb",
+var SCRIPT_URL_CMD = "curl -fsSL \"${globals.sh_script}\" -o /home/jelastic/diagnostic.sh && bash /home/jelastic/diagnostic.sh ",
     SUCCESS = "success",
     WARNING = "warning",
     NO_ERRORS_FOUND_CODE = 1000,
     ERRORS_FOUND_CODE = 1001,
+    RECOVERED_CODE =  1002,
+    ERRORS_FOUND_CODE_CMD=1,
     AUTH_ERROR_CODE = 2,
+    CMD_ERROR = 4109,
     ROOT = "root",
+    parsed = {},
+    SQLDB = "sqldb",
+    envName = "${env.name}",
+    login = "${this.login:}",
+    password = "${this.password:}",
+    isRecovering = false,
+    statuses = [],
+    bootstraps = [],
     resp,
     nodes,
     id;
+    
+var responses = [], item, obj = {};
+    
+isRecovering = !!(login && password);
+    
+function cmd(values) {
+    var resp;
+                
+    values = values || {};
+    
+    if (values.nodeid) {
+        resp = api.env.control.ExecCmdById(envName, session, values.nodeid, toJSON([{ command: values.command }]), true, ROOT);
+    } else {
+        resp = api.env.control.ExecCmdByGroup(envName, session, values.nodeGroup || SQLDB, toJSON([{ command: values.command }]), true, false, ROOT);
+    }
+    
+    return resp;
+};
 
-resp = api.env.control.ExecCmdById("${env.name}", session, ${nodes.sqldb.master.id}, toJSON([{ command: "curl -fsSL \"https://raw.githubusercontent.com/lazarenkoalexey/mysql-cluster/v2.5.0-test/scripts/db-diagnostic.sh\" -o /home/jelastic/diagnostic.sh && bash /home/jelastic/diagnostic.sh ${nodes.sqldb.length} ${this.login:} ${this.password:}" }]), true, ROOT);
+function getMySQLStatuses () {
+    if (!statuses.length) {
+        for (var i = 0, n = responses.length; i < n; i++) {
+            statuses.push(responses[i].service_status);
+        }
+    }
+    
+    return statuses;
+};
 
-if (resp.result == 4109) {
-  if (resp.responses[0].exitStatus == 2) {
+function countMatches(array, number) {
+    return array.filter(function (value) { return value == number}).length;
+};
+
+function getBootstapValues() {
+    if (!bootstraps.length) {
+        for (var i = 0, n = responses.length; i < n; i++) {
+            bootstraps.push(responses[i].bootstrap);
+        }
+    }
+    
+    return bootstraps;
+};
+
+function stopMySQLServices() {
+    return cmd({
+        nodeGroup: SQLDB,
+        command: "service mysql stop"
+    });
+};
+
+function killPIDFile() {
+    return cmd({
+        command: "rm -rf /var/lib/mysql/*.pid"
+    })
+};
+
+resp = api.env.control.GetEnvInfo(envName, session);
+if (resp.result != 0) return resp;
+
+nodes = resp.nodes;
+
+resp = cmd({
+    command: SCRIPT_URL_CMD + " ${nodes.sqldb.length}",
+    nodeGroup: SQLDB
+});
+
+if (resp.result == CMD_ERROR) {
+    if (resp.responses && resp.responses.length) {
+        
+        for (var i = 0, n = resp.responses.length; i < n; i++) {
+            
+            item = resp.responses[i];
+            var parsed = JSON.parse(item.out);
+            
+            responses.push({
+                nodeid: item.nodeid,
+                service_status: parsed.service_status,
+                size: parsed.size,
+                status: parsed.status,
+                bootstrap: parsed.bootstrap,
+                exitStatus: item.exitStatus
+            });
+        }
+    }
+}
+
+api.marketplace.console.WriteLog("responses->" + responses);
+
+            
+for (i = 0, n = responses.length; i < n; i++) {
+    item = responses[i];
+    
+    if (item.exitStatus == AUTH_ERROR_CODE) {
       return {
-        type: WARNING,  //aUTH ERROR
-        message: resp.responses[0].out
+        type: WARNING,  //AUTH ERROR
+        message: item.out
       };
-  }
+    }
       
-  if (resp.responses[0].exitStatus == 1) {
+    if (item.exitStatus == ERRORS_FOUND_CODE_CMD) {
+      
+      if (isRecovering) { //recovery action
+          //CASE 1:
+          resp = getMySQLStatuses();
+          if (countMatches(resp, 1) == "${nodes.sqldb.length}" && getBootstapValues().indexOf(1) != -1) {
+              resp = stopMySQLServices();
+              if (resp.result != 0) return resp;
+              
+              resp = killPIDFile();
+              if (resp.result != 0) return resp;
+          }
+          
+      }
+
       return {
           result: ERRORS_FOUND_CODE, //when errors were found
           type: SUCCESS
           
       };
-  }
+    }
 }
+
 
 return {
     result: NO_ERRORS_FOUND_CODE,
