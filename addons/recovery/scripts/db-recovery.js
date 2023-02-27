a833c4adff44be524da7c38da121daddc84911fd
alexey.lazarenko <dzotic9@gmail.com>
2023-02-27 12:37:07 +0200

    Update db-recovery.js

--- addons/recovery/scripts/db-recovery.js
+++ addons/recovery/scripts/db-recovery.js
@@ -28,16 +28,13 @@ function DBRecovery() {
 
     me.process = function() {
         let resp = me.defineRestore();
-        log("resp0 -> " + resp);
         if (resp.result != 0) return resp;
 
         resp = me.execRecovery();
-        log("resp1 -> " + resp);
         if (resp.result != 0) return resp;
 
         resp = me.parseResponse(resp.responses, true);
 
-        log("resp -> " + resp);
         if (isRestore) {
             let failedPrimaries = me.getFailedPrimaries();
             log("failedPrimaries.length -> " + failedPrimaries.length);
