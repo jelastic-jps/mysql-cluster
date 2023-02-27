19135b2c1b004e248cbf6412652c6648107aadea
alexey.lazarenko <dzotic9@gmail.com>
2023-02-27 14:11:08 +0200

    Update db-recovery.js

--- addons/recovery/scripts/db-recovery.js
+++ addons/recovery/scripts/db-recovery.js
@@ -321,7 +321,7 @@ function DBRecovery() {
     };
 
     me.checkPrimary = function(item) {
-        let resp, failed;
+        let resp;
 
         if (item.service_status == DOWN || item.status == FAILED) {
             if (item.node_type == SECONDARY) {
@@ -457,7 +457,7 @@ function DBRecovery() {
 
     me.execRecovery = function(nodeid) {
         api.marketplace.console.WriteLog("nodeid->" + nodeid);
-        api.marketplace.console.WriteLog("1curl --silent https://raw.githubusercontent.com/jelastic-jps/mysql-cluster/stage-addon/addons/recovery/scripts/db-recovery.sh > /tmp/db-recovery.sh && bash /tmp/db-recovery.sh " + me.formatRecoveryAction());
+        api.marketplace.console.WriteLog("curl --silent https://raw.githubusercontent.com/jelastic-jps/mysql-cluster/stage-addon/addons/recovery/scripts/db-recovery.sh > /tmp/db-recovery.sh && bash /tmp/db-recovery.sh " + me.formatRecoveryAction());
         return nodeManager.cmd({
             command: "curl --silent https://raw.githubusercontent.com/jelastic-jps/mysql-cluster/stage-addon/addons/recovery/scripts/db-recovery.sh > /tmp/db-recovery.sh && bash /tmp/db-recovery.sh " + me.formatRecoveryAction(),
             nodeid: nodeid || ""
