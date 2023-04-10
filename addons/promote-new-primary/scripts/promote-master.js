4efc7e21f40a9cc8e3342541ef49eb1869fa57c2
alexey.lazarenko <dzotic9@gmail.com>
2023-04-11 00:54:21 +0300

    Update promote-master.js (#261)

--- addons/promote-new-primary/scripts/promote-master.js
+++ addons/promote-new-primary/scripts/promote-master.js
@@ -54,7 +54,7 @@ function promoteNewPrimary() {
             };
         }
 
-        return this.cmdByGroup("touch " + TMP_FILE, PROXY);
+        return this.cmdByGroup("touch " + TMP_FILE, PROXY, 3);
     };
 
     this.setContainerVar = function() {
@@ -71,7 +71,7 @@ function promoteNewPrimary() {
     this.diagnosticNodes = function() {
         let command = "curl -fsSL 'https://github.com/jelastic-jps/mysql-cluster/raw/JE-66025/addons/recovery/scripts/db-recovery.sh' -o /tmp/db_recovery.sh\n" +
             "bash /tmp/db_recovery.sh --diagnostic"
-        let resp = this.cmdByGroup(command, SQLDB);
+        let resp = this.cmdByGroup(command, SQLDB, 60);
         this.log("diagnosticNodes resp ->" + resp);
         if (resp.result != 0) return resp;
 
@@ -197,7 +197,7 @@ function promoteNewPrimary() {
 
             let command = "bash /usr/local/sbin/jcm.sh newPrimary --server=node" + this.getNewPrimaryNode().id;
             this.log("newPrimaryOnProxy command ->" + command);
-            return this.cmdByGroup(command, PROXY);
+            return this.cmdByGroup(command, PROXY, 20);
         }
 
         return { result: 0 }
@@ -209,7 +209,7 @@ function promoteNewPrimary() {
         this.log("promoteNewSQLPrimary newPrimary ->" + newPrimary);
         let command = "curl -fsSL 'https://github.com/jelastic-jps/mysql-cluster/raw/JE-66025/addons/recovery/scripts/db-recovery.sh' -o /tmp/db_recovery.sh\n" +
             "bash /tmp/db_recovery.sh --scenario promote_new_primary";
-        let resp = this.cmdById(newPrimary.id, command);
+        let resp = this.cmdById(newPrimary.id, command, 20);
         this.log("promoteNewSQLPrimary cmdById ->" + resp);
         if (resp.result != 0) return resp;
 
@@ -284,7 +284,7 @@ function promoteNewPrimary() {
         });
         if (resp.result != 0) return resp;
 
-        return this.cmdByGroup("rm -rf " + TMP_FILE, PROXY);
+        return this.cmdByGroup("rm -rf " + TMP_FILE, PROXY, 3);
     };
 
     this.removeFailedPrimary = function() {
@@ -298,11 +298,19 @@ function promoteNewPrimary() {
         return { result: 0 }
     };
 
-    this.cmdByGroup = function(command, nodeGroup) {
+    this.cmdByGroup = function(command, nodeGroup, timeout) {
+        if (timeout) {
+            command = "timeout " + timeout + "s bash -c \"" + command + "\"";
+        }
+
         return api.env.control.ExecCmdByGroup(envName, session, nodeGroup, toJSON([{ command: command }]), true, false, ROOT);
     };
 
-    this.cmdById = function(id, command) {
+    this.cmdById = function(id, command, timeout) {
+        if (timeout) {
+            command = "timeout " + timeout + "s bash -c \"" + command + "\"";
+        }
+        
         return api.env.control.ExecCmdById(envName, session, id, toJSON([{ command: command }]), true, ROOT);
     };
 };
